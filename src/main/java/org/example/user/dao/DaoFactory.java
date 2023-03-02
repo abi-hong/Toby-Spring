8c1ebbed33e00e4bb7a9aa82319e063a5ef0cb44
abi-hong <flffjakfwm20@gmail.com>
2023-03-02 19:36:56 +0900

    의존관계 주입

--- src/main/java/org/example/user/dao/DaoFactory.java
+++ src/main/java/org/example/user/dao/DaoFactory.java
@@ -4,6 +4,7 @@ import org.example.user.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//DI 컨테이너
 @Configuration//애플리케이션 컨텍스트가 사용할 정보 표시
 public class DaoFactory {//팩토리를 통해 객체를 생성해서 반환함으로 Main에서 ConnectionMaker을 결정하는 것을 막음
 
